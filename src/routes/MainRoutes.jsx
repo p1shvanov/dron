b87f6a5fbd7467b0845d6a10d2463c410ff79fe7
Gleb <g.kurashenko@tg.dunice.net>
2024-06-09 01:55:40 +0300

    add route

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -3,6 +3,7 @@ import { lazy } from 'react';
 // project imports
 import MainLayout from 'layout/MainLayout';
 import Loadable from 'ui-component/Loadable';
+import { element } from 'prop-types';
 
 // dashboard routing
 const DashboardDefault = Loadable(lazy(() => import('views/dashboard')));
@@ -84,6 +85,10 @@ const MainRoutes = {
         {
             path: 'route-page',
             element: <RoutePage />
+        },
+        {
+            path: 'controller-page',
+            element: <Controller />
         }
     ]
 };
