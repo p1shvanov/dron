b5f999bb82fc30e7d93e8926bdedda2eb869ca76
BIRYA <BIRYA1488>
2024-06-09 04:59:54 +0300

    rewrfsdf

--- src/views/dashboard/TotalOrderLineChartCard.jsx
+++ src/views/dashboard/TotalOrderLineChartCard.jsx
@@ -16,10 +16,127 @@ import Chart from 'react-apexcharts';
 import MainCard from 'ui-component/cards/MainCard';
 import SkeletonTotalOrderCard from 'ui-component/cards/Skeleton/EarningCard';
 
-
 // assets
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import { useSelector } from 'react-redux';
+
+export const CardChart = () => {
+  return (
+    <MainCard
+      border={false}
+      content={false}
+      sx={{
+        bgcolor: 'primary.dark',
+        color: '#fff',
+        overflow: 'hidden',
+        position: 'relative',
+        '&>div': {
+          position: 'relative',
+          zIndex: 5
+        },
+        '&:after': {
+          content: '""',
+          position: 'absolute',
+          width: 210,
+          height: 210,
+          background: theme.palette.primary[800],
+          borderRadius: '50%',
+          top: { xs: -105, sm: -85 },
+          right: { xs: -140, sm: -95 }
+        },
+        '&:before': {
+          content: '""',
+          position: 'absolute',
+          width: 210,
+          height: 210,
+          background: theme.palette.primary[800],
+          borderRadius: '50%',
+          top: { xs: -155, sm: -125 },
+          right: { xs: -70, sm: -15 },
+          opacity: 0.5
+        }
+      }}
+    >
+      <Box sx={{ p: 2.25 }}>
+        <Grid container direction="column">
+          <Grid item>
+            <Grid container justifyContent="space-between">
+              <Grid item>
+                <Button
+                  disableElevation
+                  variant={timeValue ? 'contained' : 'text'}
+                  size="small"
+                  sx={{ color: 'inherit' }}
+                  onClick={(e) => handleChangeTime(e, true)}
+                >
+                  Volts
+                </Button>
+                <Button
+                  disableElevation
+                  variant={!timeValue ? 'contained' : 'text'}
+                  size="small"
+                  sx={{ color: 'inherit' }}
+                  onClick={(e) => handleChangeTime(e, false)}
+                >
+                  Ampers
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item sx={{ mb: 0.75 }}>
+            <Grid container alignItems="center">
+              <Grid item xs={6}>
+                <Grid container alignItems="center">
+                  <Grid item>
+                    {timeValue ? (
+                      <Typography sx={{ fontSize: '2.125rem', fontWeight: 500, mr: 1, mt: 1.75, mb: 0.75 }}>
+                        {voltageBatteryValue[voltageBatteryValue.length - 1]['battery_remaining']}
+                      </Typography>
+                    ) : (
+                      <Typography sx={{ fontSize: '2.125rem', fontWeight: 500, mr: 1, mt: 1.75, mb: 0.75 }}>
+                        {voltageBatteryValue[voltageBatteryValue.length - 1]['current_battery']}
+                      </Typography>
+                    )}
+                  </Grid>
+                  <Grid item>
+                    <Avatar
+                      sx={{
+                        ...theme.typography.smallAvatar,
+                        cursor: 'pointer',
+                        bgcolor: 'primary.200',
+                        color: 'primary.dark'
+                      }}
+                    >
+                      <ArrowDownwardIcon fontSize="inherit" sx={{ transform: 'rotate3d(1, 1, 1, 45deg)' }} />
+                    </Avatar>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography
+                      sx={{
+                        fontSize: '1rem',
+                        fontWeight: 500,
+                        color: 'primary.200'
+                      }}
+                    >
+                      Value
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={6}>
+                {timeValue ? (
+                  <Chart {...chartData(voltageBatteryValue, 'SYS_STATUS', 'battery_remaining', 100)} />
+                ) : (
+                  <Chart {...chartData(voltageBatteryValue, 'SYS_STATUS', 'current_battery', 4000)} />
+                )}
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
+    </MainCard>
+  );
+};
 
 // ==============================|| DASHBOARD - TOTAL ORDER LINE CHART CARD ||============================== //
 
@@ -155,7 +272,6 @@ const TotalOrderLineChartCard = ({ isLoading }) => {
               </Grid>
             </Grid>
           </Box>
-
         </MainCard>
       )}
     </>
